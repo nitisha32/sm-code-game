bcdcbc8277b12667da9bfc0fb80a3f8db52625cc
nitisha32 <nitisha.r14@gmail.com>
2023-03-24 10:32:39 +0530

    Update settings.py

--- smgamev1/settings.py
+++ smgamev1/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n$y!_504x!=p^k6!*r$u@l1$))0(&$nlzz4!c57dwyd^40!eb$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
